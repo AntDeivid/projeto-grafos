2822df1e935333888fac68c9ffebcc2929a87ade
Deivid Santos <deividsantos121@gmail.com>
2023-07-06 19:37:40 -0300

    Documentação

--- Questão 01/Questao01.cpp
+++ Questão 01/Questao01.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 bool isValidGraphColor(vector<list<int>> grafo, int start, vector<string>& colors) {
+    // Estruturas auxiliares
     list<int> fila;
     fila.push_back(start);
     colors[start] = "gray";
@@ -13,26 +14,33 @@ bool isValidGraphColor(vector<list<int>> grafo, int start, vector<string>& color
         int u = fila.front();
         fila.pop_front();
 
+        // Percorre os vértices adjacentes
         for(auto v : grafo[u]) {
+            // Se o vértice não foi visitado, colore e adiciona na fila
             if(colors[v] == "white") {
+                // Se o vértice atual for cinza, o próximo será preto, e vice-versa
                 colors[v] = (colors[u] == "gray") ? "black" : "gray";
-                fila.push_back(v);
-            } else if(colors[v] == colors[u]) {
-                return false;
+                fila.push_back(v); // Adiciona na fila
             }
+            // Se o vértice adjacente já foi visitado e possui a mesma cor do vértice atual, a coloração é inválida
+            else if(colors[v] == colors[u]) { return false; }
         }
     }
+    // Se não houver conflitos, a coloração é válida
     return true;
 }
 
 bool validGraphColor(vector<list<int>> grafo) {
+    // Estruturas auxiliares
     vector<string> colors;
     colors.resize(grafo.size(), "white");
 
+    // Percorre todos os vértices do grafo
     for(int i = 0; i < grafo.size(); i++) {
+        // Se o vértice não foi visitado, verifica se a coloração é válida
         if(colors[i] == "white") {
             if(!isValidGraphColor(grafo, i, colors)) {
-                return false;
+                return false; // Se não for, retorna falso
             }
         }
     }
@@ -41,9 +49,9 @@ bool validGraphColor(vector<list<int>> grafo) {
 
 int main() {
     fstream file;
-    file.open("NAO/snarks_28_vertices.txt");
+    file.open("SIM/cubic_bipartite_26_vertices.txt"); // Abrir arquivo
     
-    while (!file.eof()) {
+    while (!file.eof()) { 
         int n, m;
         file >> n >> m;
 
@@ -53,11 +61,12 @@ int main() {
         for(int i = 0; i < m; i++) {
             int u, v;
             file >> u >> v;
+            // Criação do grafo/lista de adjacência
             grafo[u].push_back(v);
             grafo[v].push_back(u);
         }
 
-        if(validGraphColor(grafo)) {
+        if(validGraphColor(grafo)) { // Chamada da função
             cout << "SIM" << endl;
         } else {
             cout << "NAO" << endl;
--- Questão 02/Questao02.cpp
+++ Questão 02/Questao02.cpp
@@ -9,54 +9,56 @@
 
 using namespace std;
 
+// Estrutura auxiliar para armazenar os dados de um ator
 struct Actor {
     string name;
     string movie;
     int baconNumber;
 };
 
+// Função para encontrar o número de Bacon para cada ator no grafo
 void findBaconNumber(const unordered_map<string, list<pair<string, string>>>& graph) {
+    // Estruturas auxiliares
     queue<Actor> queue;
     unordered_map<string, bool> visited;
     unordered_map<string, int> baconNumbers;
 
+    // Inicializa as estruturas auxiliares
     for (const auto& entry : graph) {
-        visited[entry.first] = false;
-        baconNumbers[entry.first] = -1;
+        visited[entry.first] = false; // Marca todos os atores como não visitados
+        baconNumbers[entry.first] = -1; // Inicializa o número de Bacon de todos os atores como -1 (não calculado)
     }
 
-    vector<Actor> actors;
+    vector<Actor> actors; // Vetor para armazenar os atores
 
     queue.push({ "Kevin Bacon", "", 0 });
-    visited["Kevin Bacon"] = true;
     baconNumbers["Kevin Bacon"] = 0;
 
     while (!queue.empty()) {
         Actor current = queue.front();
         queue.pop();
-
-        /*if (current.name != "Kevin Bacon") {
-            actors.push_back(current);
-        }*/
         
         actors.push_back(current);
-
+        // Percorre os atores adjacentes
         for (const auto& coactor : graph.at(current.name)) {
-            string nextActor = coactor.first;
+            string nextActor = coactor.first; // Próximo ator = ator adjacente
             string movie = coactor.second;
 
+            // Se o ator não foi visitado, adiciona na fila e marca como visitado
             if (!visited[nextActor]) {
-                queue.push({ nextActor, movie, current.baconNumber + 1 });
+                queue.push({ nextActor, movie, current.baconNumber + 1 }); // Adiciona na fila, incrementando o número de Bacon
                 visited[nextActor] = true;
-                baconNumbers[nextActor] = current.baconNumber + 1;
+                baconNumbers[nextActor] = current.baconNumber + 1; // Atualiza o número de Bacon
             }
         }
     }
 
+    // Ordena os atores por ordem alfabética
     sort(actors.begin(), actors.end(), [](const Actor& a, const Actor& b) {
         return a.name < b.name;
     });
 
+    // Imprime os atores, seus números de Bacon e por qual filme eles se conectam
     for (const auto& actor : actors) {
         cout << "O numero de Bacon de " << actor.name << " e " << actor.baconNumber << " pelo filme " << actor.movie << endl;
     }
@@ -64,24 +66,27 @@ void findBaconNumber(const unordered_map<string, list<pair<string, string>>>& gr
 
 int main() {
     ifstream file("input.txt");
-    if (!file.is_open()) {
+    if (!file.is_open()) { // Verifica se o arquivo foi aberto corretamente
         cout << "Erro ao abrir o arquivo" << endl;
         return 0;
     }
 
-    unordered_map<string, list<pair<string, string>>> graph;
+    unordered_map<string, list<pair<string, string>>> graph; // Cria a estrutura de dados para armazenar o grafo
 
     string line;
     while (getline(file, line)) {
         string actor, movie, coactor;
 
+        // Encotra os delimitadores
         size_t firstDelimiter = line.find(';');
         size_t secondDelimiter = line.find(';', firstDelimiter + 1);
 
+        // Extrai os dados da linha, separando-os pelos delimitadores
         actor = line.substr(0, firstDelimiter);
         movie = line.substr(firstDelimiter + 1, secondDelimiter - firstDelimiter - 1);
         coactor = line.substr(secondDelimiter + 1);
 
+        // Adiciona os atores e o filme no grafo, fazendo o grafo ser não-direcionado
         graph[actor].push_back({ coactor, movie });
         graph[coactor].push_back({ actor, movie });
     }
